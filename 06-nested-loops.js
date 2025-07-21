e9f69962c58c93d612de0cc1c177c65450a2fb50
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-07-21 12:57:06 +0530

    star pattern problems

--- 06-nested-loops.js
+++ 06-nested-loops.js
@@ -1,9 +1,50 @@
 let prompt = require('prompt-sync')(); // npm i prompt-sync
-let n = prompt('enter a number: ');
+let n = Number(prompt('enter a number: '));
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j <= i; j++) {
-   process.stdout.write('* ');
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(String.fromCharCode(64 + j) + " ");
+//   }
+//   console.log();
+// }
+
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = n; j >= i; j--) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if (j <= n - i) {
+//       process.stdout.write(" ");
+//     } else {
+//       process.stdout.write("* ");
+//     }
+//   }
+//   console.log();
+// }
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if (i == j || i + j == n + 1) {
+//       process.stdout.write("* ");
+//     } else {
+//       process.stdout.write("  ");
+//     }
+//   }
+//   console.log();
+// }
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= (n * 2) - 1; j++) {
+    if (i == j || i + j == n * 2) {
+      process.stdout.write("* ");
+    } else {
+      process.stdout.write("  ");
+    }
   }
   console.log();
 }
