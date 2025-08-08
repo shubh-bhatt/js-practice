44f9cfa600cd4da0ee30d01e9743738c9ff23f08
Shubham Bhatt <shubhambhatt92021@gmail.com>
2025-08-09 00:41:32 +0530

    user input

--- 01-user_input/script.js
+++ 01-user_input/script.js
@@ -1,6 +1,6 @@
 let username;
 
 document.getElementById("btn").onclick = function () {
-  let username = document.getElementById("myInput").value;
+  username = document.getElementById("myInput").value;
   document.getElementById("myH1").textContent = `Hello ${username}`;
 };
